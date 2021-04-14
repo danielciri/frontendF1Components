9eccc78275fc1751a152b17c6ea39aef35d43d15
Daniel Cirilo <dcirilol@everis.com>
2021-04-14 11:34:28 +0200

    Fixed problem with the table driver-card-all-races

--- src/components/my-component/driver-card-all-races/driver-card-all-races.tsx
+++ src/components/my-component/driver-card-all-races/driver-card-all-races.tsx
@@ -13,36 +13,27 @@ export class DriverCardAllRaces {
     return (
 
 
-      <div class="card race">
-        <div class="header header-race" >
-          <div class="container">
-            <div class="row">
-              <div class="col">
-                <h6>Race</h6>
-              </div>
-              <div class="col">
-                <h6>Position</h6>
-              </div>
-              <div class="col">
-                <h6>Time</h6>
-              </div>
+      <div class="contentTable">
+                <table class="">
+                    <thead>
+                        <tr>
+     
+                            <th>Race</th>
+                            <th>Rank</th>
+                            <th>Time</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="table-active">
+                            <td>{this.race}</td>
+                            <td>{this.position}</td>
+                            <td>{this.time}</td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
             </div>
-          </div>
-        </div>
-        <div class="card-body daniel">
-          <table class="table table-bordered table-striped" >
-            <tbody class="tbody races">
-            <td>{this.race}</td>
-            <td>{this.position}</td>
-            <td>{this.time}</td>
-            <br></br>
-            <br></br>
-            </tbody>
-           
-          </table>
-        
-        </div>
-      </div>
 
     );
   }
